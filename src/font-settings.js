39966daa5c507b67f1d823aa79199b3cabe35533
huapohen <694450321@qq.com>
2026-09-16 13:22:17 +0800

    Slightly dim sidebar foreground and clarify color setting

--- src/font-settings.js
+++ src/font-settings.js
@@ -1,4 +1,4 @@
-export const fonts={overviewWidth:50,size:13,terminalSize:13,sidebarSize:13,family:'Menlo',cursorColor:'#c678dd',treeColor:'#abb2bf',markdownColor:'#b0b0b0',headingColor:'#8abebf'};
+export const fonts={overviewWidth:50,size:13,terminalSize:13,sidebarSize:13,family:'Menlo',cursorColor:'#c678dd',treeColor:'#a3aab7',markdownColor:'#b0b0b0',headingColor:'#8abebf'};
 export function applyFonts(value={}){
  if(Number.isFinite(value.overviewWidth)&&value.overviewWidth>=20&&value.overviewWidth<=200)fonts.overviewWidth=value.overviewWidth;
  document.documentElement.style.setProperty('--overview-width',fonts.overviewWidth+'px');
